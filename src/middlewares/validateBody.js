e07a9b847eb7a84098105d53215dd3a9316edf81
VanyaUzhorod <131401354+VanyaUzhorod@users.noreply.github.com>
2025-05-09 21:23:37 +0300

    fix cloud

--- src/middlewares/validateBody.js
+++ src/middlewares/validateBody.js
@@ -13,3 +13,12 @@ export const validateBody = (schema) => async (req, res, next) => {
     next(error);
   }
 };
+// export const validateBody = (schema) => {
+//   return (req, res, next) => {
+//     const { error } = schema.validate(req.body);
+//     if (error) {
+//       return res.status(400).json({ message: error.message });
+//     }
+//     next();
+//   };
+// };
